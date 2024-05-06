0ec70c3ecabeeade24ca8f30078cdcd9da25b4c5
sanderMarcusChristensen <144114615+sanderMarcusChristensen@users.noreply.github.com>
2024-05-06 13:03:39 +0200

    add Calculator and a connetionpool

---
+++ src/main/java/app/Calculator.java
@@ -0,0 +1,18 @@
+package app;
+
+public class Calculator {
+
+    public double getTotalCarportPrice(){
+
+        return 0;
+
+    }
+
+    public double getWoodLengthPrice(){
+        return 0;
+    }
+
+    public double calculateWhatINeed(){
+        return 0;
+    }
+}
