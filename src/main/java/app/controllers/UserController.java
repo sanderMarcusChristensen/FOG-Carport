fd195804c46a97c9daa2b6262a7dc35a9df720c8
sanderMarcusChristensen <144114615+sanderMarcusChristensen@users.noreply.github.com>
2024-05-06 13:00:45 +0200

    user mapper and controller

--- src/main/java/app/controllers/UserController.java
+++ src/main/java/app/controllers/UserController.java
@@ -1,4 +1,13 @@
 package app.controllers;
 
+import app.persistence.ConnectionPool;
+import io.javalin.Javalin;
+
 public class UserController {
+
+    public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
+
+    }
+
+
 }
--- src/main/java/app/persistence/UserMapper.java
+++ src/main/java/app/persistence/UserMapper.java
@@ -1,4 +1,67 @@
 package app.persistence;
 
+import app.entities.User;
+import app.exceptions.DatabaseException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import io.javalin.http.Context;
+
 public class UserMapper {
+    private static final List<User> userList = new ArrayList<>();
+    static User currentUser;
+
+    public static User login(String userName, String password, ConnectionPool connectionPool) throws DatabaseException {
+        String sql = "select * from public.\"users\" where user_name=? and user_password=?";
+
+        try (
+                Connection connection = connectionPool.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql)
+        ) {
+            ps.setString(1, userName);
+            ps.setString(2, password);
+
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                int id = rs.getInt("user_id");
+                String role = rs.getString("user_role");
+                int wallet = rs.getInt("user_wallet");
+                return new User(id, userName, password, role);
+            } else {
+                throw new DatabaseException("Fejl i login. Prøv igen");
+            }
+        } catch (SQLException e) {
+            throw new DatabaseException("DB fejl", e.getMessage());
+        }
+    }
+
+    public static void createuser(String userName, String password, ConnectionPool connectionPool) throws DatabaseException {
+        String sql = "insert into users (user_name, user_password, user_role) values (?,?,?)";
+
+        try (
+                Connection connection = connectionPool.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql)
+        ) {
+            ps.setString(1, userName);
+            ps.setString(2, password);
+            ps.setString(3, "user");
+
+            int rowsAffected = ps.executeUpdate();
+            if (rowsAffected != 1) {
+                throw new DatabaseException("Fejl ved oprettelse af ny bruger");
+            }
+        } catch (SQLException e) {
+            String msg = "Der er sket en fejl. Prøv igen";
+            if (e.getMessage().startsWith("ERROR: duplicate key value ")) {
+                msg = "Brugernavnet findes allerede. Vælg et andet";
+            }
+            throw new DatabaseException(msg, e.getMessage());
+        }
+    }
 }
