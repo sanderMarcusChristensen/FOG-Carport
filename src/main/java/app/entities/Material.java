a569f5ce06a84a97d2a796e93d483eb2751919d1
sanderMarcusChristensen <144114615+sanderMarcusChristensen@users.noreply.github.com>
2024-05-06 13:03:18 +0200

    material entitites and mapper

---
+++ src/main/java/app/entities/Material.java
@@ -0,0 +1,102 @@
+package app.entities;
+
+public class Material {
+
+    private int material_id;
+    private String type;
+    private double width;
+    private double height;
+    private int amount;
+    private double price;
+    private String description;
+    private int unit;
+
+    public Material(int material_id, String type, double width, double height, int amount, double price, String description, int unit) {
+        this.material_id = material_id;
+        this.type = type;
+        this.width = width;
+        this.height = height;
+        this.amount = amount;
+        this.price = price;
+        this.description = description;
+        this.unit = unit;
+    }
+
+    public int getMaterial_id() {
+        return material_id;
+    }
+
+    public void setMaterial_id(int material_id) {
+        this.material_id = material_id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getUnit() {
+        return unit;
+    }
+
+    public void setUnit(int unit) {
+        this.unit = unit;
+    }
+
+    @Override
+    public String toString() {
+        return "Material{" +
+                "material_id=" + material_id +
+                ", type='" + type + '\'' +
+                ", width=" + width +
+                ", height=" + height +
+                ", amount=" + amount +
+                ", price=" + price +
+                ", description='" + description + '\'' +
+                ", unit=" + unit +
+                '}';
+    }
+}
