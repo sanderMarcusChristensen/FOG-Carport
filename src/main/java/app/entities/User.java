078e04db8c861848047b9713f4eb943324ad3630
MateenCPH <cph-mr645@cphbusiness.dk>
2024-05-10 14:51:49 +0200

    update to include address from db + getter/setter + toString

--- src/main/java/app/entities/User.java
+++ src/main/java/app/entities/User.java
@@ -8,62 +8,59 @@ public class User {
     private String user_password;
     private String zipcode;
     private String user_role;
+    private String user_address;
 
-    public User(int user_id, String user_name, String user_email, String user_password,String zipcode, String user_role) {
+    public User(int user_id, String user_name, String user_password, String user_email, String zipcode, String user_role, String user_address) {
         this.user_id = user_id;
         this.user_name = user_name;
         this.user_email = user_email;
         this.user_password = user_password;
         this.zipcode = zipcode;
         this.user_role = user_role;
+        this.user_address = user_address;
     }
 
     public String getZipcode() {
         return zipcode;
     }
-
     public void setZipcode(String zipcode) {
         this.zipcode = zipcode;
     }
-
     public int getUser_id() {
         return user_id;
     }
-
     public void setUser_id(int user_id) {
         this.user_id = user_id;
     }
-
     public String getUser_name() {
         return user_name;
     }
-
     public void setUser_name(String user_name) {
         this.user_name = user_name;
     }
-
     public String getUser_password() {
         return user_password;
     }
-
     public void setUser_password(String user_password) {
         this.user_password = user_password;
     }
-
     public String getUser_role() {
         return user_role;
     }
-
     public void setUser_role(String user_role) {
         this.user_role = user_role;
     }
-
     public String getUser_email() {
         return user_email;
     }
-
     public void setUser_email(String user_email) {
         this.user_email = user_email;
+    }
+    public String getUser_address() {
+        return user_address;
+    }
+    public void setUser_address(String user_address) {
+        this.user_address = user_address;
     }
 
     @Override
@@ -71,8 +68,11 @@ public class User {
         return "User{" +
                 "user_id=" + user_id +
                 ", user_name='" + user_name + '\'' +
+                ", user_email='" + user_email + '\'' +
                 ", user_password='" + user_password + '\'' +
+                ", zipcode='" + zipcode + '\'' +
                 ", user_role='" + user_role + '\'' +
+                ", user_address='" + user_address + '\'' +
                 '}';
     }
 }
