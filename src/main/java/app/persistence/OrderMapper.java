d73afaa0765bc662d47cc91fc1c2275c8d15aefd
MateenCPH <cph-mr645@cphbusiness.dk>
2024-05-13 22:49:36 +0200

    done - matches jons video

--- src/main/java/app/persistence/OrderMapper.java
+++ src/main/java/app/persistence/OrderMapper.java
@@ -1,6 +1,6 @@
 package app.persistence;
 
-import app.entities.Order;
+import app.entities.*;
 import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
 
@@ -16,11 +16,11 @@ import java.util.List;
 public class OrderMapper {
 
 
-    public static List<Order> getAllOrder(ConnectionPool connectionPool) throws DatabaseException {
+    public static List<Order> getAllOrders(ConnectionPool connectionPool) throws DatabaseException {
 
         List<Order> orderList = new ArrayList<>();
 
-        String sql = "select * FORM order";
+        String sql = "SELECT * FROM order INNER JOIN users using (user_id)";
 
         try (
                 Connection connection = connectionPool.getConnection();
@@ -29,20 +29,74 @@ public class OrderMapper {
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
 
-                int order_id = rs.getInt("order_id");
-                int total_price = rs.getInt("total_price");
-                boolean status = rs.getBoolean("status");
-                double height = rs.getDouble("height");
-                double width = rs.getDouble("width");
-                double length = rs.getDouble("length");
+                int userId = rs.getInt("user_id");
+                String userName = rs.getString("user_name");
+                String userPassword = rs.getString("user_password");
+                String userEmail = rs.getString("user_email");
+                String userZipCode = rs.getString("user_zipcode");
+                String userRole = rs.getString("user_role");
+                String userAddress = rs.getString("user_address");
+                int orderId = rs.getInt("order_id");
+                double carportWidth = rs.getDouble("carport_width");
+                double carportLength = rs.getDouble("carport_length");
                 Date date = rs.getDate("date");
-                int user_id = rs.getInt("user_id");
-                orderList.add(new Order(order_id, total_price, status, height, width, length, date, user_id));
+                boolean status = rs.getBoolean("status");
+                int totalPrice = rs.getInt("total_price");
+                User user = new User(userId, userName, userPassword, userEmail, userZipCode, userRole, userAddress);
+                Order order = new Order(orderId, carportWidth, carportLength, date, status, totalPrice, user);
             }
         } catch (SQLException e) {
             throw new DatabaseException("Error while retrieving orders: " + e.getMessage());
         }
 
         return orderList;
+    }
+
+    public static List<OrderItem> getOrderItemsByOrderId(int orderId, ConnectionPool connectionPool) throws DatabaseException {
+        List<OrderItem> orderItemList = new ArrayList<>();
+
+        String sql = "SELECT * FROM bill_of_materials_view WHERE order_id = ?";
+
+        try (
+                Connection connection = connectionPool.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql)
+        ) {
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+
+                //Order
+                double carportWidth = rs.getDouble("carport_width");
+                double carportLength = rs.getDouble("carport_length");
+                Date date = rs.getDate("date");
+                boolean status = rs.getBoolean("status");
+                int userId = rs.getInt("user_id");
+                double totalPrice = rs.getDouble("total_price");
+                Order order = new Order(orderId, carportWidth, carportLength, date, status, totalPrice, null);
+
+                //Product
+                int productId = rs.getInt("product_id");
+                String name = rs.getString("name");
+                String unit = rs.getString("unit");
+                double price = rs.getDouble("price");
+                Product product = new Product(productId, name, unit, price);
+
+                //Product variant
+                int productVariantId = rs.getInt("product_variant_id");
+                String description = rs.getString("description");
+                int length = rs.getInt("length");
+                ProductVariant productVariant = new ProductVariant(productVariantId, product, length);
+
+                //OrderItem
+                int orderItemId = rs.getInt("order_item_id");
+                int quantity = rs.getInt("quantity");
+                OrderItem orderItem = new OrderItem(orderItemId, order, productVariant, quantity, description);
+
+                orderItemList.add(orderItem);
+            }
+        } catch (SQLException e) {
+            throw new DatabaseException("Could not get data from the database", e.getMessage());
+        }
+
+        return orderItemList;
     }
 }
