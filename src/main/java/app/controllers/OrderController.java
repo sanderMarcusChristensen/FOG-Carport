2e95f767dec5f78594d52ba330dbedb441c7bec6
MateenCPH <cph-mr645@cphbusiness.dk>
2024-05-15 20:56:52 +0200

    send request method done. sendrequestloggedin method almost done - needs to not cretae user

--- src/main/java/app/controllers/OrderController.java
+++ src/main/java/app/controllers/OrderController.java
@@ -13,7 +13,8 @@ import app.persistence.ConnectionPool;
 import app.persistence.UserMapper;
 import io.javalin.Javalin;
 
-import java.util.Date;
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.Locale;
 
 public class OrderController {
@@ -21,8 +22,8 @@ public class OrderController {
         app.post("continueToPageTwo", ctx -> customCarport_pagetwo(ctx));
         app.get("continueToPageThree", ctx -> ctx.render("customCarport_3.html"));
         app.get("backToPageOne", ctx -> customCarport_pageone(ctx));
-        app.post("createRequest", ctx -> createRequestAndUser(ctx, connectionPool));
-
+        app.post("sendRequest", ctx -> sendRequest(ctx, connectionPool));
+        app.get("sendRequestLoggedIn", ctx -> sendRequestLoggedIn(ctx, connectionPool));
         app.post("getCarportSize", ctx -> showPreView(ctx));
         app.get("/customCarportPreView", ctx -> OrderController.showPreView(ctx));
     }
@@ -33,89 +34,153 @@ public class OrderController {
 
     public static void customCarport_pagetwo(Context ctx) {
 
-        int carportWidth = Integer.parseInt(ctx.formParam("width_id"));
-        int carportLength = Integer.parseInt(ctx.formParam("length_id"));
+        int carportWidth = Integer.parseInt(ctx.formParam("width"));
+        int carportLength = Integer.parseInt(ctx.formParam("length"));
         String carportRoof = ctx.formParam("roof_id");
 
-        ctx.sessionAttribute("carportWidth", carportWidth);
-        ctx.sessionAttribute("carportLength", carportLength);
+        ctx.sessionAttribute("width", carportWidth);
+        ctx.sessionAttribute("length", carportLength);
         ctx.sessionAttribute("carportRoof", carportRoof);
 
         ctx.render("customCarport_2.html");
     }
 
-    public static void createRequestAndUser(Context ctx, ConnectionPool connectionPool) {
+//    public static void createRequestAndUser(Context ctx, ConnectionPool connectionPool) {
+//
+//        //Henter alle informationer vedrørende custom carport fra den tidligere forespørgsel
+//        int carportWidth = ctx.sessionAttribute("carportWidth");
+//        int carportLength = ctx.sessionAttribute("carportLength");
+//        String carportRoof = ctx.sessionAttribute("carportRoof");
+//
+//        String name = ctx.formParam("name");
+//        String email = ctx.formParam("email");
+//        String password = ctx.formParam("password");
+//        String passwordrepeat = ctx.formParam("passwordrepeat");
+//        String address = ctx.formParam("address");
+//        int zipcode = Integer.parseInt(ctx.formParam("zipcode"));
+//        String comment = ctx.formParam("comment");
+//
+//        if (password.equals(passwordrepeat) && name.length() > 3 && password.length() > 3 && passwordrepeat.length() > 3) {
+//            try {
+//                UserMapper.insertUser(name, password, email, zipcode, address, connectionPool);
+//                ctx.attribute("usermessage", "Du er hermed oprettet med e-mail: " + email + " - Du vil modtage en email " +
+//                        "med yderligere informationer vedr. din forespørgsel.");
+//                ctx.render("customCarport_3.html");
+//            } catch (DatabaseException e) {
+//                ctx.attribute("errormessage", "Din email findes allerede. Prøv igen eller log ind");
+//                ctx.render("customCarport_3.html");
+//            }
+//        } else {
+//            ctx.attribute("errormessage", "Noget gik galt. Prøv igen. Vær sikker på følgende:\n- Password længde skal være mere end 3 tegn\n- Dine passwords skal matche i begge felter - Email skal indeholde '@' - Dit postnummer skal være gyldigt");
+//            ctx.render("customCarport_3.html");
+//        }
+//    }
 
-        //Henter alle informationer vedrørende custom carport fra den tidligere forespørgsel
-        int carportWidth = ctx.sessionAttribute("carportWidth");
-        int carportLength = ctx.sessionAttribute("carportLength");
-        String carportRoof = ctx.sessionAttribute("carportRoof");
-
-        String name = ctx.formParam("name");
-        String email = ctx.formParam("email");
-        String password = ctx.formParam("password");
-        String passwordrepeat = ctx.formParam("passwordrepeat");
-        String address = ctx.formParam("address");
-        int zipcode = Integer.parseInt(ctx.formParam("zipcode"));
-        String comment = ctx.formParam("comment");
-
-        if (password.equals(passwordrepeat) && name.length() > 3 && password.length() > 3 && passwordrepeat.length() > 3) {
-            try {
-                UserMapper.createuser(name, password, email, zipcode, address, connectionPool);
-                ctx.attribute("usermessage", "Du er hermed oprettet med e-mail: " + email + " - Du vil modtage en email " +
-                        "med yderligere informationer vedr. din forespørgsel.");
-                ctx.render("customCarport_3.html");
-            } catch (DatabaseException e) {
-                ctx.attribute("errormessage", "Din email findes allerede. Prøv igen eller log ind");
-                ctx.render("customCarport_3.html");
-            }
-        } else {
-            ctx.attribute("errormessage", "Noget gik galt. Prøv igen. Vær sikker på følgende:\n- Password længde skal være mere end 3 tegn\n- Dine passwords skal matche i begge felter - Email skal indeholde '@' - Dit postnummer skal være gyldigt");
-            ctx.render("customCarport_3.html");
-        }
-    }
-
-    public static void showPreView(Context ctx){
+    public static void showPreView(Context ctx) {
         int width = Integer.parseInt(ctx.formParam("width"));
         int length = Integer.parseInt(ctx.formParam("length"));
+
+        ctx.sessionAttribute("width", width);
+        ctx.sessionAttribute("length", length);
+
         String type = ctx.sessionAttribute("type");
 
         Locale.setDefault(new Locale("US"));
         CarportSvg svg = new CarportSvg(width, length);
 
-        Svg carportSvg = new Svg(0, 0, "0 0 855 690", "100%" );
-        carportSvg.addRectangle(0, 0, width, length, "stroke:#000000; stroke-width:2px; fill: #ffffff; margin: auto;" );
+        Svg carportSvg = new Svg(0, 0, "0 0 855 690", "100%");
+        carportSvg.addRectangle(0, 0, width, length, "stroke:#000000; stroke-width:2px; fill: #ffffff; margin: auto;");
 
         ctx.attribute("svg", svg.toString());
         ctx.render("customCarportPreView.html");
 
     }
 
-    private static void sendRequest(Context ctx, ConnectionPool connectionPool){
+    private static void sendRequest(Context ctx, ConnectionPool connectionPool) {
 
         //Get order details from front-end
-        int width = ctx.sessionAttribute("width");
-        int length = ctx.sessionAttribute("length");
-        Date date = ctx.sessionAttribute("date");
+        int carportWidth = ctx.sessionAttribute("width");
+        int carportLength = ctx.sessionAttribute("length");
+        //Set date of today
+        Date date = Date.valueOf(LocalDate.now());
         int status = 1;
         int totalPrice = 19999;
-        User user = new User(1,"gud", "1234", "sanderc69@gmail.com", "2990", "user", "Nivå");   // hard-code for nu (laver en dummy user)
+
+        String name = ctx.formParam("name");
+        String password = ctx.formParam("password");
+        String passwordrepeat = ctx.formParam("passwordrepeat");
+        String email = ctx.formParam("email");
+        int zipcode = Integer.parseInt(ctx.formParam("zipcode"));
+        String address = ctx.formParam("address");
+
+        //User user = new User();   // hard-code for nu (laver en dummy user)
 
 
-        Order order = new Order(0,width,length,date,status,totalPrice,user);
+        if (password.equals(passwordrepeat) && name.length() > 3 && password.length() > 3 && passwordrepeat.length() > 3) {
+            try {
 
-        try{
+                User user = new User(0, name, password, email, zipcode, "user", address);
+                user = UserMapper.insertUser(user, connectionPool);
+
+                Order order = new Order(0, carportWidth, carportLength, date, status, totalPrice, user);
+                order = OrderMapper.insertOrder(order, connectionPool);
+
+                //calculate order items (stykliste)
+                Calculator calculator = new Calculator(carportWidth, carportLength, connectionPool);
+                calculator.calcCarport(order);
+
+                //save order items in database (stykeliste)
+                OrderMapper.insertOrderItems(calculator.getOrderItem(), connectionPool);
+
+                ctx.attribute("usermessage", "Du er hermed oprettet med e-mail: " + email + " - Du vil modtage en email " +
+                        "med yderligere informationer vedr. din forespørgsel.");
+                ctx.render("customCarport_3.html");
+                //create messge to customer and render order / request confirmation
+                //ctx.render("ItEllerAndetFed/dude");   // Lave om, idk
+
+            } catch (DatabaseException e) {
+                throw new RuntimeException(e);
+            }
+
+
+        } else {
+            ctx.attribute("errormessage", "Noget gik galt. Prøv igen. Vær sikker på følgende:\n- Password længde skal være mere end 3 tegn\n- Dine passwords skal matche i begge felter - Email skal indeholde '@' - Dit postnummer skal være gyldigt");
+            ctx.render("customCarport_3.html");
+        }
+    }
+
+    private static void sendRequestLoggedIn(Context ctx, ConnectionPool connectionPool) {
+
+        //Get order details from front-end
+        int carportWidth = ctx.sessionAttribute("width");
+        int carportLength = ctx.sessionAttribute("length");
+        //Set date of today
+        Date date = Date.valueOf(LocalDate.now());
+        int status = 1;
+        int totalPrice = 19999;
+
+        //User user = new User();   // hard-code for nu (laver en dummy user)
+
+        try {
+
+            User user = ctx.sessionAttribute("currentUser");
+            user = UserMapper.insertUser(user, connectionPool);
+
+            Order order = new Order(0, carportWidth, carportLength, date, status, totalPrice, user);
             order = OrderMapper.insertOrder(order, connectionPool);
 
             //calculate order items (stykliste)
-            Calculator calculator = new Calculator(width,length,connectionPool);
+            Calculator calculator = new Calculator(carportWidth, carportLength, connectionPool);
             calculator.calcCarport(order);
 
             //save order items in database (stykeliste)
             OrderMapper.insertOrderItems(calculator.getOrderItem(), connectionPool);
 
+            ctx.attribute("usermessage", "Du vil modtage en email på " + user.getUserEmail() +
+                    "med yderligere informationer vedr. din forespørgsel.");
+            ctx.render("customCarport_3.html");
             //create messge to customer and render order / request confirmation
-            ctx.render("ItEllerAndetFed/dude");   // Lave om, idk
+            //ctx.render("ItEllerAndetFed/dude");   // Lave om, idk
 
         } catch (DatabaseException e) {
             throw new RuntimeException(e);
@@ -124,3 +189,4 @@ public class OrderController {
 
     }
 }
+
