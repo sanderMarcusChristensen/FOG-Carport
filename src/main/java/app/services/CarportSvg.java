ae8817a506d3c4341b457f6d688c63d310a9a576
Marcus Rasmussen <marcusphr1808@gmail.com>
2024-05-15 12:20:58 +0200

    added cross og post metoder (men de er ikke done)

--- src/main/java/app/services/CarportSvg.java
+++ src/main/java/app/services/CarportSvg.java
@@ -11,21 +11,36 @@ public class CarportSvg {
         this.length = length;
         carportSvg = new Svg(0, 0, "0 0 855 690", "100%");
         carportSvg.addRectangle(0, 0, length, width, "stroke:#000000; stroke-width:2px; fill: #ffffff");
+        //carportSvg.addArrow(0, length, width, 0, "stroke:#000000; stroke-width:2px; fill: #ffffff");
+        //carportSvg.addCross(1,2,3,4, "style=\"stroke:#000000; stroke-dasharray: 5 5;");
         addBeams();
         addRafters();
+        //addCross();
     }
 
-    private void addBeams(){
+    private void addBeams(){ // -- remme
         //<rect x="0" y="35" height="4.5" width="780" style="stroke:#000000; fill: #ffffff" />
         //<rect x="0" y="565" height="4.5" width="780" style="stroke:#000000; fill: #ffffff" />
         carportSvg.addRectangle(0, 35, 4.5, width, "stroke:#000000; stroke-width:2px; fill: #ffffff");
-        carportSvg.addRectangle(0, 565, 4.5, width, "stroke:#000000; stroke-width:2px; fill: #ffffff");
+        carportSvg.addRectangle(0, length - 35, 4.5, width, "stroke:#000000; stroke-width:2px; fill: #ffffff");
     }
 
-    private void addRafters(){
+    private void addRafters(){ // -- spær
         for(double i = 0; i < width ; i += (int) 55.714){
             carportSvg.addRectangle(i, 0, length, 4.5, "stroke:#000000; fill: #ffffff");
         }
+    }
+
+    private void addCross(){ // -- kryds
+        //<line x1="55" y1="35" x2="600" y2="569.5" style="stroke:#000000; stroke-dasharray: 5 5;" />
+        //<line x1="55" y1="569.5" x2="600" y2="35" style="stroke:#000000; stroke-dasharray: 5 5;" />
+
+        carportSvg.addCross(55, 35, 600, 569, "style=\"stroke:#000000; stroke-dasharray: 5 5;");
+        carportSvg.addCross(55, 569, 600, 35, "style=\"stroke:#000000; stroke-dasharray: 5 5;");
+    }
+
+    private void addPost(){ // -- stolper
+
     }
 
     public void setLength(int length) {
