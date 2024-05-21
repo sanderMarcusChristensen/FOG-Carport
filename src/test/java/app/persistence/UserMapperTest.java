2db18652f829d01f4e7dde6e6c5ed5d22f9efb76
MateenCPH <cph-mr645@cphbusiness.dk>
2024-05-21 17:15:36 +0200

    updated testclasses

--- src/test/java/app/persistence/UserMapperTest.java
+++ src/test/java/app/persistence/UserMapperTest.java
@@ -13,7 +13,12 @@ import java.sql.Statement;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserMapperTest {
-    private static final ConnectionPool connectionPool = ConnectionPool.getInstance("", "", "", "");
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "Teknologisk2023!";
+    private static final String URL = "jdbc:postgresql://209.38.202.233:5432/%s?currentSchema=test";
+    private static final String DB = "carport_jon";
+
+    private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
 
 
     @BeforeAll
